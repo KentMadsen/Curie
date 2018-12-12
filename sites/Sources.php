51504f93aea94869e48eede88b10ae4608d40509
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2018-12-13 00:06:00 +0100

    automatic batch script

---
+++ sites/Sources.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+
+        <meta name="viewport"
+              content="width=device-width, initial-scale=1.0">
+        
+        <meta http-equiv="X-UA-Compatible"
+              content="ie=edge">
+        
+        <?php
+            include './generics/Fonts.html';
+            include './generics/Libraries.html';
+            include './generics/Content.html';
+            include './generics/Style.html';
+        ?>
+
+        <title>
+            Space Education: Our Story
+        </title>
+    </head>
+    <body>
+        <?php
+            include './Header.php';
+        ?>
+        <main>
+
+        </main>
+        <?php
+            include './generics/Footer.html';
+        ?>
+    </body>
+</html>
