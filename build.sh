3665b3d31890f4e0b58d8b6df9cb9c8f42f1e883
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2018-12-12 22:46:14 +0100

    automatic batch script

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-echo 'building : ./sites/css/status.css' >> ./build.txt
-sass ./scss/status.scss ./sites/css/status.css  >> ./build.txt
 
 echo 'building : ./sites/css/style.css' >> ./build.txt
 sass ./scss/style.scss ./sites/css/style.css  >> ./build.txt
