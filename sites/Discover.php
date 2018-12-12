9ff71f28453b14448f9042dda274d8785af271fc
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2018-12-13 00:25:20 +0100

    automatic batch script

--- sites/Discover.php
+++ sites/Discover.php
@@ -7,7 +7,7 @@
             include './generic_head.php';
         ?>
 
-        title>
+        <title>
             <?php
                 include './functions/statements.php';
                 printTitle('Discover'); 
