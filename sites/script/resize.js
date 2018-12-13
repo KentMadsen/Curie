02c513b0dee4ecf7350147e9cb21fae95c992bca
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2018-12-13 06:00:28 +0100

    automatic batch script

--- sites/script/resize.js
+++ sites/script/resize.js
@@ -1,11 +1,13 @@
 function resize_cover_index()
 {
+    /*
     var element = document.getElementById('cover-container-homepage');
     var parent = element.parentElement;
     console.log(parent);
 
     setWidth(element, parent.clientWidth);
     setHeight(element, parent.clientWidth);
+    */
 }
 
 function setWidth(element, size)
