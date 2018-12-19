4af9db0937c9ab8582e07380be19f94a842833d5
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2018-12-19 18:19:43 +0100

    automatic batch script

--- sites/index.php
+++ sites/index.php
@@ -8,13 +8,13 @@
 
         <meta http-equiv="X-UA-Compatible"
               content="ie=edge">
-
-        <link rel="stylesheet"
-              href="./styles/style.css">
-
+              
         <title>
             CURIE
         </title>
+                
+        <link rel="stylesheet"
+              href="./styles/style.css">
     </head>
 
     <body>
